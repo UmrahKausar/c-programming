0d146189d6744c4b6cabb54b3c83bd036cb75109
Umrah Kausar <umrahkausar@Umrahs-MacBook-Air.local>
2025-04-28 01:52:37 +0530

    basic programming

---
+++ Calculator/Calculator.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2;
+    char operator;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+    
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch (operator) {
+        case '+':
+            printf("Result: %.2lf\n", num1 + num2);
+            break;
+        case '-':
+            printf("Result: %.2lf\n", num1 - num2);
+            break;
+        case '*':
+            printf("Result: %.2lf\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("Result: %.2lf\n", num1 / num2);
+            } else {
+                printf("Error! Division by zero.\n");
+            }
+            break;
+        default:
+            printf("Invalid operator!\n");
+            break;
+    }
+
+    return 0;
+}
